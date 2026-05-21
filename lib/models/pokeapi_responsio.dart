a3c92c6c9bbc4a35422f8295e7c33dee45aca973
Inés Mena <imena.estudiante@gmail.com>
2026-05-21 17:35:57 +0200

    imengri: Json de pokemon en formato dart

---
+++ lib/models/pokeapi_responsio.dart
@@ -0,0 +1,48 @@
+
+class Welcome {
+    final int count;
+    final String next;
+    final dynamic previous;
+    final List<Result> results;
+
+    Welcome({
+        required this.count,
+        required this.next,
+        required this.previous,
+        required this.results,
+    });
+
+    factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        count: json["count"],
+        next: json["next"],
+        previous: json["previous"],
+        results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "count": count,
+        "next": next,
+        "previous": previous,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+    };
+}
+
+class Result {
+    final String name;
+    final String url;
+
+    Result({
+        required this.name,
+        required this.url,
+    });
+
+    factory Result.fromJson(Map<String, dynamic> json) => Result(
+        name: json["name"],
+        url: json["url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "name": name,
+        "url": url,
+    };
+}
