44487773b5246b6d8afd2b29f6b83febbebc70ab
Inés Mena <imena.estudiante@gmail.com>
2026-05-24 21:05:30 +0200

    imegri: Petición http y mapeado de la respuesta

--- lib/services/pokemons_service.dart
+++ lib/services/pokemons_service.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-
+import 'package:flu_avm/mappers/pokemon_mapper.dart';
 class PokemonService {
 
   static getPokemon<String>(String pokemonId) async {
@@ -9,9 +9,11 @@ class PokemonService {
     try {
 
       final responsio = await dio.get(' https://pokeapi.co/api/v2/pokemon/$pokemonId');
-      
+      final pokemon = PokemonMapper.pokeApiPokemonToEntity(responsio.data);
+      return (pokemon, 'Data obtenida correctamente');
+
     } catch (e) {
-      
+      return (null, 'No se pudo obtener el Pokemon');
     }
 
     
